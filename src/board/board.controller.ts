e82033094ff6b5829511bbf26a4db576d2633963
banggeunho <panggeunho@gmail.com>
2023-12-24 02:20:33 +0900

    board api 구현 및 swagger 적용

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -7,31 +7,36 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { BoardService } from './board.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('board')
+@ApiTags('Board')
 export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
+
   @Get()
   findAll() {
-    return 'findAll';
+    return this.boardService.findAll();
   }
 
   @Get(':id')
   find(@Param('id') id: number) {
-    return `find ${id}`;
+    return this.boardService.findById(Number(id));
   }
 
   @Post()
   create(@Body() data) {
-    return `create post ${data}`;
+    return this.boardService.create(data);
   }
 
   @Put(':id')
   update(@Param('id') id: number, @Body() data) {
-    return `update ${id} ${data}`;
+    return this.boardService.update(Number(id), data);
   }
 
   @Delete(':id')
   remove(@Param('id') id: number) {
-    return `delete ${id}`;
+    return this.boardService.remove(Number(id));
   }
 }
---
+++ src/board/board.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class BoardService {
+  private boards = [
+    {
+      id: 1,
+      title: 'hello',
+      content: 'Content 1',
+    },
+    {
+      id: 2,
+      title: 'world',
+      content: 'Content 2',
+    },
+    {
+      id: 3,
+      title: 'fufufu',
+      content: 'Content 3',
+    },
+    {
+      id: 4,
+      title: 'boat',
+      content: 'Content 4',
+    },
+    {
+      id: 5,
+      title: 'kakao',
+      content: 'Content 5',
+    },
+    {
+      id: 6,
+      title: 'seoul',
+      content: 'Content 6',
+    },
+  ];
+  findAll() {
+    return this.boards;
+  }
+
+  findById(id: number) {
+    return this.boards.find((board) => board.id === id);
+  }
+
+  create(data) {
+    const newBoard = {
+      id: this.boards.length + 1,
+      ...data,
+    };
+    this.boards = [...this.boards, newBoard];
+    return this.boards;
+  }
+
+  update(id: number, data) {
+    const board = this.findById(id);
+
+    board.content = data.contents;
+    board.title = data.name;
+
+    return this.boards;
+  }
+
+  remove(id: number) {
+    const index: number = this.boards.findIndex((board) => board.id === id);
+
+    if (index < 0) {
+      return null;
+    }
+
+    this.boards.splice(index, 1);
+    return this.boards;
+  }
+}
