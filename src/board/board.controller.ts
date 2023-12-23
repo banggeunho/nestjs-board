fc04cd180ec00b6338ecb59a68c33093bd464608
banggeunho <panggeunho@gmail.com>
2023-12-24 01:33:01 +0900

    add controller

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+  @Get()
+  findAll() {
+    return 'findAll';
+  }
+
+  @Get(':id')
+  find(@Param('id') id: number) {
+    return `find ${id}`;
+  }
+
+  @Post()
+  create(@Body() data) {
+    return `create post ${data}`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() data) {
+    return `update ${id} ${data}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return `delete ${id}`;
+  }
+}
