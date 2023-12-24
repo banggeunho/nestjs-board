f4afe769f95594b796511d4b648b1a035a59e8e2
banggeunho <panggeunho@gmail.com>
2023-12-24 21:58:18 +0900

    board 관련 CRUD 작업 완료

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -16,38 +16,6 @@ export class BoardService {
     private boardRepository: Repository<BoardEntity>,
   ) {}
 
-  private boards = [
-    {
-      id: 1,
-      title: 'hello',
-      content: 'Content 1',
-    },
-    {
-      id: 2,
-      title: 'world',
-      content: 'Content 2',
-    },
-    {
-      id: 3,
-      title: 'fufufu',
-      content: 'Content 3',
-    },
-    {
-      id: 4,
-      title: 'boat',
-      content: 'Content 4',
-    },
-    {
-      id: 5,
-      title: 'kakao',
-      content: 'Content 5',
-    },
-    {
-      id: 6,
-      title: 'seoul',
-      content: 'Content 6',
-    },
-  ];
   findAll() {
     return this.boardRepository.find();
   }
@@ -73,23 +41,29 @@ export class BoardService {
     return this.boardRepository.save(data);
   }
 
-  update(id: number, data: BoardUpdateDto) {
-    // const board = this.findById(id);
+  async update(id: number, data: BoardUpdateDto) {
+    const board = await this.getBoardById(id);
 
-    // board.contents = data.contents;
-    // board.title = data.title;
-
-    return this.boards;
-  }
-
-  remove(id: number) {
-    const index: number = this.boards.findIndex((board) => board.id === id);
-
-    if (index < 0) {
-      return null;
+    if (!board) {
+      throw new HttpException('Data Not Found', HttpStatus.NOT_FOUND);
     }
 
-    this.boards.splice(index, 1);
-    return this.boards;
+    return this.boardRepository.update(id, {
+      ...data,
+    });
+  }
+
+  async remove(id: number) {
+    const board = await this.getBoardById(id);
+
+    if (!board) {
+      throw new HttpException('Data Not Found', HttpStatus.NOT_FOUND);
+    }
+
+    return this.boardRepository.remove(board);
+  }
+
+  private async getBoardById(id: number) {
+    return await this.boardRepository.findOneBy({ id });
   }
 }
--- src/board/dto/board-update.dto.ts
+++ src/board/dto/board-update.dto.ts
@@ -9,5 +9,3 @@ export class BoardUpdateDto {
   @IsOptional()
   contents?: string;
 }
-
-// export class BoardUpdateDto extends PartialType(BoardCreateDto) {}
